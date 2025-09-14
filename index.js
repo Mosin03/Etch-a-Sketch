0b2c756f1cb63a3053399b795874bf42efa6755b
Mosin03 <mohsinali102703@gmail.com>
2025-09-14 00:15:46 -0400

    Was able to get the layout down for a 16x16 box

---
+++ index.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+const style = getComputedStyle(container);
+containerHeight = parseFloat(style.height);
+containerWidth = parseFloat(style.width);
+
+// Create the 16x16 using a for loop 
+
+for (let i = 0; i < (16* 16); i++){
+ let div =  document.createElement("div");
+ div.style.height = `${(containerHeight / 16)}px`;
+ div.style.width = `${(containerWidth / 16)}px`;
+ container.appendChild(div);
+}
