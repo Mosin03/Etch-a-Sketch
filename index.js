f31a7be208c438a91b6fa00045102a6ed2aff5ee
Mosin03 <mohsinali102703@gmail.com>
2025-09-15 10:56:31 -0400

    Have a working eventlistener that changes the color of the div

--- index.js
+++ index.js
@@ -10,4 +10,16 @@ for (let i = 0; i < (16* 16); i++){
  div.style.height = `${(containerHeight / 16)}px`;
  div.style.width = `${(containerWidth / 16)}px`;
  container.appendChild(div);
+ div.addEventListener('mouseover', ()=>{
+  div.style.backgroundColor = "green";
+ })
 }
+
+// const gridBoxes = document.querySelectorAll(".container div");
+
+
+// gridBoxes.forEach((box) => {
+//   box.addEventListener('mouseover', ()=>{
+//     box.style.backgroundColor = "red";  
+//   })
+// })
